767d886fd43e92307c9b319f7a6f35cb74b6aa17
KingVg-py <131527840+KingVg-py@users.noreply.github.com>
2023-04-22 12:27:22 +0100

    Add files via upload

---
+++ palindrome or not.py
@@ -0,0 +1,13 @@
+word=input("Enter a word, and I will determine if it is a palindrome or not: ")
+
+reverse=""
+
+for x in range(len(word)-1, -1, -1):
+    reverse+=word[x]
+
+print(f"In reverse it is {reverse}")
+
+if reverse==word:
+    print("It is a palindrome!")
+else:
+    print("It is not a palindrome!")
